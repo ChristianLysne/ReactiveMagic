18cb0788591b0432092146509771820d7fbe6b48
Christian Lysne <Christian.Lysne@nrk.no>
2016-04-15 12:40:04 +0200

    Initial commit - Added project with RxSwift and RxCocoa

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+target 'ReactiveMagic' do
+    pod 'RxSwift', '~> 2.4'
+    pod 'RxCocoa', '~> 2.4'
+end
+
+target 'ReactiveMagicTests' do
+
+end
+
+target 'ReactiveMagicUITests' do
+
+end
+
---
+++ ReactiveMagic/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ReactiveMagic
+//
+//  Created by Christian Lysne on 15.04.2016.
+//  Copyright © 2016 NRK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
